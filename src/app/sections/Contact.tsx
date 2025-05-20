e8cdd6622b814be910e49ea2af2dd69eb5f79831
Fernando Jacome <fernanjacome02@gmail.com>
2025-05-20 13:45:31 -0500

    cambio de planetas 7

--- src/app/sections/Contact.tsx
+++ src/app/sections/Contact.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { FaGithub, FaLinkedin, FaEnvelope, FaPhoneAlt, FaFileDownload } from 'react-icons/fa';
+import {
+    FaGithub,
+    FaLinkedin,
+    FaEnvelope,
+    FaPhoneAlt,
+    FaWhatsapp,
+    FaFileDownload
+} from 'react-icons/fa';
 import styles from './Contact.module.css';
 
 export default function Contact() {
@@ -13,23 +20,54 @@ export default function Contact() {
                 </p>
 
                 <div className={styles.grid}>
-                    <a href="https://www.linkedin.com/in/fernando-j%C3%A1come-v%C3%A9liz-408b7a281/" target="_blank" className={styles.item}>
+                    <a
+                        href="https://www.linkedin.com/in/fernando-j%C3%A1come-v%C3%A9liz-408b7a281/"
+                        target="_blank"
+                        className={styles.item}
+                    >
                         <FaLinkedin className={styles.icon} />
                         <span>LinkedIn</span>
                     </a>
-                    <a href="https://github.com/fernanjacome" target="_blank" className={styles.item}>
+
+                    <a
+                        href="https://github.com/fernanjacome"
+                        target="_blank"
+                        className={styles.item}
+                    >
                         <FaGithub className={styles.icon} />
                         <span>GitHub</span>
                     </a>
-                    <a href="mailto:fernanjacome0sb@gmail.com" className={styles.item}>
+
+                    <a
+                        href="mailto:fernanjacome0sb@gmail.com"
+                        className={styles.item}
+                    >
                         <FaEnvelope className={styles.icon} />
                         <span>fernanjacome0sb@gmail.com</span>
                     </a>
-                    <a href="tel:+593959928535" className={styles.item}>
+
+                    <a
+                        href="tel:+593959928535"
+                        className={styles.item}
+                    >
                         <FaPhoneAlt className={styles.icon} />
-                        <span>+593 95 992 8535</span>
+                        <span>Llamar</span>
                     </a>
-                    <a href="/FernandoJacome_CV.pdf" download className={styles.item}>
+
+                    <a
+                        href="https://wa.me/593959928535"
+                        target="_blank"
+                        className={styles.item}
+                    >
+                        <FaWhatsapp className={styles.icon} />
+                        <span>WhatsApp</span>
+                    </a>
+
+                    <a
+                        href="/FernandoJacome_CV.pdf"
+                        download
+                        className={styles.item}
+                    >
                         <FaFileDownload className={styles.icon} />
                         <span>Descargar CV</span>
                     </a>
